31fccf2641ff0e7dc051cf546e657b30cbb7caf7
David Norton <david.william.norton@gmail.com>
2025-07-26 02:13:30 -0400

    Update Contact section: simplify to just email contact info

--- src/lib/InfoOverlay.svelte
+++ src/lib/InfoOverlay.svelte
@@ -980,27 +980,7 @@
         <h2>Contact</h2>
         <div class="overlay__section-text">
           <p>
-            Get in touch at <a href="mailto:hi@queeringthemap.com"
-              >hi@queeringthemap.com</a
-            >
-          </p>
-          <p>
-            Stay connected with us on <a
-              href="https://www.instagram.com/queeringthemap/"
-              target="_blank"
-              rel="noopener">Instagram</a
-            >,
-            <a
-              href="https://twitter.com/queeringthemap_"
-              target="_blank"
-              rel="noopener">Twitter</a
-            >
-            and
-            <a
-              href="https://www.tiktok.com/@queering_the_map"
-              target="_blank"
-              rel="noopener">TikTok</a
-            >.
+            Get in touch at david [dot] norton [at] notastranger [dot] org.
           </p>
         </div>
       </section>
